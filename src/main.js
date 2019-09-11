ec8e648182abf9bb5ca858b39a21c257676b7d07
unknown <[shrestha_sl@hotmail.com]>
2019-09-10 19:55:40 -0500

    Commiting Initial code

---
+++ src/main.js
@@ -0,0 +1,117 @@
+const students = [
+    {
+        name: "Chris Miller",
+        subject: "History",
+        info: "Failed last exam",
+        score: 59
+    },
+    {
+        name: "Courtney Seward",
+        subject: "History",
+        info: "Has completed all homework",
+        score: 91
+    },
+    {
+        name: "Garrett Ward",
+        subject: "History",
+        info: "Wonderful at helping other students",
+        score: 88
+    },
+    {
+        name: "John Dulaney",
+        subject: "History",
+        info: "Has never missed a class or exam",
+        score: 92
+    },
+    {
+        name: "Greg Lawrence",
+        subject: "History",
+        info: "Sub-par performance all around",
+        score: 64
+    },
+    {
+        name: "Leah Duvic",
+        subject: "History",
+        info: "Wonderful student",
+        score: 97
+    },
+    {
+        name: "Jesse Page",
+        subject: "History",
+        info: "Smokes too much. Distracting.",
+        score: 76
+    },
+    {
+        name: "Kevin Haggerty",
+        subject: "History",
+        info: "Falls asleep in class",
+        score: 79
+    },
+    {
+        name: "Max Wolf",
+        subject: "History",
+        info: "Talks too much",
+        score: 83
+    },
+    {
+        name: "Lissa Goforth",
+        subject: "History",
+        info: "Asks pointless, unrelated questions",
+        score: 78
+    },
+    {
+        name: "Tyler Bowman",
+        subject: "History",
+        info: "When was the last time he attended class?",
+        score: 48
+    },
+    {
+        name: "Ray Medrano",
+        subject: "History",
+        info: "Needs to contribute to in-class discussions",
+        score: 95
+    }
+]
+
+
+const createStudentComponent = (name, student, info) => {
+    return `
+    <div class="student">
+    <h4>Name: ${name}</h4>
+    <p>Subject: ${student}</p>
+    <p>Note: ${info}</p>
+    <hr>
+    </div>
+    `
+}
+
+
+
+    
+const studentContainer = document.querySelector("#container")
+for (let i = 0; i < students.length; i++) {
+    const student = students[i]
+    studentContainer.innerHTML += createStudentComponent (
+        student.name,
+        student.subject,
+        student.info
+    )
+}
+
+const lissa = createStudentComponent(
+    "Lissa Goforth",
+    "History",
+    "Asks pointless, unrelated questions")
+
+const courtney = createStudentComponent(
+    "Courtney Seward",
+    "History",
+    "Has completed all homework")
+
+const greg = createStudentComponent(
+    "Greg Lawrence",
+    "History",
+    "Sub-par performance all around")
+
+
+   
